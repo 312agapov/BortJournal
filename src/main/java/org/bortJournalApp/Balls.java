fb48c363af9afd86d721ab51df3cf11d17bf2a1d
LEONID <312agapov@gmai.com>
2024-05-23 09:37:19 +0400

    why commit?

--- src/main/java/org/bortJournalApp/Balls.java
+++ src/main/java/org/bortJournalApp/Balls.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.bortJournalApp;
 
 public class Balls {
 
--- src/main/java/org/bortJournalApp/Main.java
+++ src/main/java/org/bortJournalApp/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.bortJournalApp;
 
 public class Main {
     public static void main(String[] args) {
